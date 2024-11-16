2ece7b7ecb2c002e2f5a27335eda804b5e45be32
Julien Vital <julien.vital@yahoo.fr>
2024-11-16 19:51:20 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import glsl from 'vite-plugin-glsl'
 
 export default {
-
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+    build: {
+        outDir: 'dist' // Dossier de sortie
     },
     plugins:
     [
